44a31fc09f0e1f4f5d78c5c3fe1e52a33656b04a
Lucila Kim <lucila.kim.9@gmail.com>
2022-12-22 18:42:58 -0300

    Add the class assignment to the end of the js file

--- tarea/task-2-class6-salary-form-bootstrap/task-2-class-10-salary-scripts.js
+++ tarea/task-2-class6-salary-form-bootstrap/task-2-class-10-salary-scripts.js
@@ -149,9 +149,7 @@ function cleanUpInput(numbers) {
 
 /*
 TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
-
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+Implementar bootstrap en los ejercicios anteriores
+En los formularios que crearon para calcular edades, integrantes, etc. darles estilo con Bootstrap.
 */
 
